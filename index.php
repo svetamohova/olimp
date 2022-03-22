c3952b3b6603f57fd5ac0fcfabd84cb8216fb5b2
Sveta Mohova <37954140+svetamohova@users.noreply.github.com>
2022-03-22 23:08:56 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+
+<head>
+    <!-- Local style sheet relative to this file -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container" id="content">
+        <div class="block">
+            100
+        </div>
+        <div class="block">
+            200
+        </div>
+        <div class="block">
+            300
+        </div>
+
+
+        <div class="row">
+            <div class="col">1 of 2</div>
+            <div class="col">2 of 2</div>
+        </div>
+    </div>
+</body>
+
+</html>
